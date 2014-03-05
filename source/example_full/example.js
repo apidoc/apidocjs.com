f01178100eaadffbfc20f0a275a9e69ec8afac16
Peter Rottmann <rottmann@inveris.de>
2014-03-05 13:25:14 +0100

    Extend example with Object and Array definition.

--- source/example_full/example.js
+++ source/example_full/example.js
@@ -9,9 +9,19 @@
  * 
  * @apiParam {String} id The Users-ID.
  *
- * @apiSuccess {String} id         The Users-ID.
- * @apiSuccess {Date}   registered Registration Date. 
- * @apiSuccess {Date}   name       Fullname of the User.
+ * @apiExample Example usage:
+ * curl -i http://localhost/user/4711
+ *
+ * @apiSuccess {String}   id            The Users-ID.
+ * @apiSuccess {Date}     registered    Registration Date. 
+ * @apiSuccess {Date}     name          Fullname of the User.
+ * @apiSuccess {String[]} nicknames     List of Users nicknames (Array of Strings).
+ * @apiSuccess {Object}   profile       Profile data (example for an Object)
+ * @apiSuccess {Number}   profile.age   Users age.
+ * @apiSuccess {String}   profile.image Avatar-Image.
+ * @apiSuccess {Object[]} options       List of Users options (Array of Objects).
+ * @apiSuccess {String}   options.name  Option Name.
+ * @apiSuccess {String}   options.value Option Value.
  *
  * @apiError NoAccessRight Only authenticated Admins can access the data.
  * @apiError UserNotFound   The <code>id</code> of the User was not found.
